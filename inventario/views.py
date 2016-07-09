f114d98f6c109e12427d17f4a693217d667e3834
vrobles <vanessita.robles@gmail.com>
2016-07-09 16:49:44 -0500

    Cambio de error

--- inventario/views.py
+++ inventario/views.py
@@ -3,4 +3,4 @@ from .models import Inventario
 # Create your views here.
 def post_list(request):
 	posts = Inventario.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'inventario/inventario_list.html', {'posts': posts})
+	return render(request, 'inventario/inventario_list.html', {'posts': posts})
