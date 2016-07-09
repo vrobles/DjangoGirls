d4acba1bb65754aff450258a8a852ab1e55f5b02
vrobles <vanessita.robles@gmail.com>
2016-07-09 16:52:58 -0500

    Cambio

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Inventario
+from django.utils import timezone
 # Create your views here.
 def post_list(request):
 	posts = Inventario.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
