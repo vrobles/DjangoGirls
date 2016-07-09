6cb3eeaf1dcb2c5513d4edbe7e12088fac2794e5
vrobles <vanessita.robles@gmail.com>
2016-07-09 16:39:32 -0500

    Modificaciones de la vista

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Inventario
 # Create your views here.
 def post_list(request):
-    return render(request, 'inventario/inventario_list.html', {})
+	posts = Inventario.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'inventario/inventario_list.html', {'posts': posts})
